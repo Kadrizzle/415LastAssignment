677c64596ce0589cfbd746e985c7e3b44f1c1997
Kadrizzle <lanekade8@gmail.com>
2024-04-29 00:25:31 -0500

    deleted duplicate variable

--- ThreadApp.js
+++ ThreadApp.js
@@ -103,7 +103,6 @@ app.all("/afterLoginSubmit", function (req, res) {
 
   const { ObjectId } = require("mongodb");
 
-  const { ObjectId } = require("mongodb");
 
   app.get("/topic/:topicId", function(req, res) {
     const topicObjectId = req.params.topicId;
