e77d5b5a83bbcf3dedc4d3c452aa81d7f823b378
Kadrizzle <lanekade8@gmail.com>
2024-04-30 11:47:29 -0500

    observer pattern

--- ThreadApp.js
+++ ThreadApp.js
@@ -1,10 +1,10 @@
 const { MongoClient, ObjectId } = require("mongodb");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const session = require('express-session');
+const session = require("express-session");
 
-
-const uri = "mongodb+srv://Kadazzle:kadizzleinthehizzle@cluster0.xgaaecl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  "mongodb+srv://Kadazzle:kadizzleinthehizzle@cluster0.xgaaecl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 class Database {
   constructor() {
@@ -30,26 +30,26 @@ class Database {
 }
 
 class Observer {
-  notify() {} 
+  notify(data) {
+    this.Observer.forEach((observer) => observer(data));
+  }
 }
-
 
 class Topic {
   constructor() {
     this.observers = [];
   }
 
-
   addObserver(observer) {
     this.observers.push(observer);
   }
 
   removeObserver(observer) {
-    this.observers = this.observers.filter(obs => obs !== observer);
+    this.observers = this.observers.filter((obs) => obs !== observer);
   }
 
   notifyObservers() {
-    this.observers.forEach(observer => observer.notify());
+    this.observers.forEach((observer) => observer.notify());
   }
 }
 
@@ -69,11 +69,13 @@ app.listen(port, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({
-  secret: 'secret-key',
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.get("/", function (req, res) {
   var mycookies = req.cookies;
@@ -86,13 +88,16 @@ app.get("/", function (req, res) {
 });
 
 app.all("/login", function (req, res) {
-  var loginString = '<style>';
-  loginString += 'body { font-size: 18px; display: flex; justify-content: center; align-items: center; height: 100vh; font-family: Arial, sans-serif; }';
-  loginString += 'form { text-align: center; }';
-  loginString += 'input[type="text"], input[type="submit"] { margin-top: 10px; padding: 10px; width: 200px; font-size: 16px; }';
-  loginString += 'a { display: inline-block; margin-top: 20px; font-size: 16px; }';
-  loginString += '</style>';
-  
+  var loginString = "<style>";
+  loginString +=
+    "body { font-size: 18px; display: flex; justify-content: center; align-items: center; height: 100vh; font-family: Arial, sans-serif; }";
+  loginString += "form { text-align: center; }";
+  loginString +=
+    'input[type="text"], input[type="submit"] { margin-top: 10px; padding: 10px; width: 200px; font-size: 16px; }';
+  loginString +=
+    "a { display: inline-block; margin-top: 20px; font-size: 16px; }";
+  loginString += "</style>";
+
   loginString += '<form action="/afterLoginSubmit" method="POST">';
   loginString += "<h1>LOGIN</h1>";
   loginString += "<label>Username: </label>";
@@ -102,7 +107,7 @@ app.all("/login", function (req, res) {
   loginString += '<br><input type="submit" value="Submit"><br>';
   loginString += '<a href="/">Go back to homepage</a><br><br>';
   loginString += "</form>";
-  
+
   res.send(loginString);
 });
 
@@ -124,14 +129,24 @@ app.all("/afterLoginSubmit", function (req, res) {
       });
 
       const allTopics = await topics.find({}).toArray(); // Fetch all topics
-      let topicsHtml = allTopics.map(topic => `
+      let topicsHtml = allTopics
+        .map(
+          (topic) => `
           <h1>${topic.TitleOfTopic} Topic</h1>
           <div style="margin-bottom: 20px;">
               <a href="/topic/${topic._id}">
-              <img src="${topic.TitleOfTopic === 'Coding' ? 'https://wallpapers.com/images/hd/coding-background-9izlympnd0ovmpli.jpg' : 'https://www.hartz.com/wp-content/uploads/2020/03/3270011244_Hartz_Disposable_Dog_Diapers_large_dogs_1300x1300.jpg'}" alt="${topic.TitleOfTopic}" style="max-width: 20%; height: auto; margin-right: 20px; border: 2px solid #000; padding: 5px;">
+              <img src="${
+                topic.TitleOfTopic === "Coding"
+                  ? "https://wallpapers.com/images/hd/coding-background-9izlympnd0ovmpli.jpg"
+                  : "https://www.hartz.com/wp-content/uploads/2020/03/3270011244_Hartz_Disposable_Dog_Diapers_large_dogs_1300x1300.jpg"
+              }" alt="${
+            topic.TitleOfTopic
+          }" style="max-width: 20%; height: auto; margin-right: 20px; border: 2px solid #000; padding: 5px;">
               </a>
           </div>
-      `).join('');
+      `
+        )
+        .join("");
 
       const addTopicForm = `
       <div style="text-align: center;">
@@ -145,14 +160,14 @@ app.all("/afterLoginSubmit", function (req, res) {
 
       res.cookie("user", username, { maxAge: 86400000, httpOnly: true });
       res.send(
-          `<h1>Welcome ${username}</h1>` +
+        `<h1>Welcome ${username}</h1>` +
           `${topicsHtml}` +
           `${addTopicForm}` +
           '<p style="text-align: center;"><a href="/" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: white; text-decoration: none; border-radius: 5px;">Go back to homepage</a></p>'
       );
     } catch (error) {
-        console.error("Error during database operation", error);
-        res.status(500).send("Internal Server Error: " + error.message);
+      console.error("Error during database operation", error);
+      res.status(500).send("Internal Server Error: " + error.message);
     }
   }
 
@@ -180,7 +195,7 @@ app.post("/addTopic", function (req, res) {
   run().catch(console.dir);
 });
 
-app.get("/topic/:topicId", function(req, res) {
+app.get("/topic/:topicId", function (req, res) {
   const topicObjectId = req.params.topicId;
   const client = databaseInstance.getClient();
 
@@ -191,15 +206,21 @@ app.get("/topic/:topicId", function(req, res) {
       const topics = database.collection("Topics");
       const topicMessages = database.collection("TopicMessages");
 
-      const topic = await topics.findOne({_id: new ObjectId(topicObjectId)});
+      const topic = await topics.findOne({ _id: new ObjectId(topicObjectId) });
       if (!topic) {
-        res.send("Topic not found <br><a href='/afterLoginSubmit'>Back to topics</a>");
+        res.send(
+          "Topic not found <br><a href='/afterLoginSubmit'>Back to topics</a>"
+        );
         return;
       }
 
       // Fetch messages that match the topicId
-      const allTopicMessages = await topicMessages.find({ TopicId: new ObjectId(topicObjectId) }).toArray();
-      let topicMessagesHtml = allTopicMessages.map(topicMessage => `<p>${topicMessage.Message}</p>`).join('');
+      const allTopicMessages = await topicMessages
+        .find({ TopicId: new ObjectId(topicObjectId) })
+        .toArray();
+      let topicMessagesHtml = allTopicMessages
+        .map((topicMessage) => `<p>${topicMessage.Message}</p>`)
+        .join("");
 
       // Display the topic and its messages
 
@@ -216,18 +237,22 @@ app.get("/topic/:topicId", function(req, res) {
 
       topicPageContent += "<script>";
       topicPageContent += "function showTextBox() {";
-      topicPageContent += "document.getElementById('textBoxDiv').style.display = 'block';";
+      topicPageContent +=
+        "document.getElementById('textBoxDiv').style.display = 'block';";
       topicPageContent += "}";
       topicPageContent += "function submitText() {";
-      topicPageContent += "var submittedText = document.getElementById('textBox').value;";
-      topicPageContent += "var submittedTextDiv = document.getElementById('submittedText');";
-      topicPageContent += "submittedTextDiv.innerHTML += '<p style=\"font-size: 20px; margin: 5px 0;\">' + submittedText + '</p>';";  // Increased font size here
+      topicPageContent +=
+        "var submittedText = document.getElementById('textBox').value;";
+      topicPageContent +=
+        "var submittedTextDiv = document.getElementById('submittedText');";
+      topicPageContent +=
+        "submittedTextDiv.innerHTML += '<p style=\"font-size: 20px; margin: 5px 0;\">' + submittedText + '</p>';"; // Increased font size here
       topicPageContent += "}";
       topicPageContent += "</script>";
 
       topicPageContent += `<p style="text-align:center;"><a href="/afterLoginSubmit" style="background-color: #4CAF50; color: white; padding: 15px 30px; text-align: center; text-decoration: none; display: inline-block; font-size: 20px; border-radius: 5px; cursor: pointer;">Back to topics</a></p>`;
-      
-      topicObserver.notify();
+
+      topicObserver.notify("user is now on topic page");
 
       res.send(topicPageContent);
     } catch (error) {
@@ -239,13 +264,13 @@ app.get("/topic/:topicId", function(req, res) {
   run().catch(console.dir);
 });
 
-app.all("/topic/:topicId/messageSubmit", function(req, res){
+app.all("/topic/:topicId/messageSubmit", function (req, res) {
   const topicObjectId = req.params.topicId;
   const messageInserted = req.params.message;
   const client = databaseInstance.getClient();
 
-  async function run(){
-    try{
+  async function run() {
+    try {
       await client.connect();
       const database = client.db("MongoTestPub");
       const message = database.collection("TopicMessages");
@@ -263,7 +288,6 @@ app.all("/topic/:topicId/messageSubmit", function(req, res){
   run().catch(console.dir);
   // res.redirect("/topic/${topicObjectId}");
 });
-
 
 app.all("/register", function (req, res) {
   var registerString = '<form action="/afterRegisterSubmit" method="POST">';
@@ -295,7 +319,9 @@ app.all("/afterRegisterSubmit", function (req, res) {
       };
 
       await parts.insertOne(doc);
-      res.send("<p>You are now registered into the database!</p><a href='/'>Go back to homepage</a>");
+      res.send(
+        "<p>You are now registered into the database!</p><a href='/'>Go back to homepage</a>"
+      );
     } finally {
       await client.close();
     }
